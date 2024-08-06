f2b91026e5a7b385260e56b674605e098ea06cfa
MDN-DA <naveedm@programmer.net>
2024-08-06 15:32:23 +0100

    refresh notes

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-print("Hello world!")
-print("I do need change my career, transition activate!")
+print("Hello World!")
+print("I DO need change (life) career, transition activate!")
 print("Paris 2024, so amazing!")
 print("COYG")
+
+print(2024 + 2025 - 3333)
+print("hello world".upper())
+
+print(len("Hello World!"))
+print(len("01001010001010100101010010101011101101101010100101"))
+
+print("Hello World"[1])
+print("Hello World"[8])
