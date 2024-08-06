40bdd9305f080ee90c41c53ac21c5ff0ea2433ee
MDN-DA <naveedm@programmer.net>
2024-08-06 17:02:23 +0100

    refresh notes

--- test.py
+++ test.py
@@ -2,12 +2,17 @@
 # print("I DO need change (life) career, transition activate!")
 # print("Paris 2024, so amazing!")
 # print("COYG")
-
+#
 # print(2024 + 2025 - 3333)
 # print("hello world".upper())
 #
-print(len("Hello World!"))
-print(len("01001010001010100101010010101011101101101010100101"))
+# print(len("Hello World!"))
+# print(len("01001010001010100101010010101011101101101010100101"))
+# #
+# print("Hello World"[1])
+# print("Hello World"[8])
 #
-print("Hello World"[1])
-print("Hello World"[8])
+print("the quick brown fox".upper())
+print("THE QUICK BROWN FOX".lower())
+print("the quick brown fox".capitalize())
+print("The Quick Brown fox fox fox".count("fox"))
