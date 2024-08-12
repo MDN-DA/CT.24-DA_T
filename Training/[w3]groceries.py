438057cdbd1c8b961d7c3311eac3c326e59cc818
MDN-DA <naveedm@programmer.net>
2024-08-12 12:35:33 +0100

    1208

--- Training/[w3]groceries.py
+++ Training/[w3]groceries.py
@@ -8,9 +8,9 @@ cost_apples = num_apples * cost_per_apple
 cost_bananas = num_bananas * cost_per_banana
 total_cost = cost_apples + cost_bananas
 #
-cost_apples_in_pounds = cost_apples / 100
-cost_bananas_in_pounds = cost_bananas / 100
-total_cost_in_pounds = total_cost / 100
+cost_apples_in_pounds = cost_apples/100
+cost_bananas_in_pounds = cost_bananas/100
+total_cost_in_pounds = total_cost/100
 #
 print(f"Total cost of the apples is: £{cost_apples_in_pounds:.2f}")
 print(f"Total cost of the bananas is: £{cost_bananas_in_pounds:.2f}")
--- Training/[w3]var.py
+++ Training/[w3]var.py
@@ -1,33 +1,33 @@
-print("All around the world"[7].upper())
-#
-my_name = "Gunners"
-print(my_name)
-print(my_name)
-print(my_name)
-print(my_name)
-#
-my_name = "Naveed"
-my_age = "36"
-is_student = True
-print("Naveed has completed the course")
-is_student = False
-print("My name is", my_name)
-#
-fav_hot_drink = "Espresso"
-fav_cold_drink = "Exotic Smoothie (Mango, Pineapple, Papapya)"
-print("My favorite drink is", fav_cold_drink)
-print(f"My name is {my_name}, my favorite drink is {fav_cold_drink}, my age is {my_age}")
+# print("All around the world"[7].upper())
+# #
+# my_name = "Gunners"
+# print(my_name)
+# print(my_name)
+# print(my_name)
+# print(my_name)
+# #
+# my_name = "Naveed"
+# my_age = "36"
+# is_student = True
+# print("Naveed has completed the course")
+# is_student = False
+# print("My name is", my_name)
+# #
+# fav_hot_drink = "Espresso"
+# fav_cold_drink = "Exotic Smoothie (Mango, Pineapple, Papapya)"
+# print("My favorite drink is", fav_cold_drink)
+# print(f"My name is {my_name}, my favorite drink is {fav_cold_drink}, my age is {my_age}")
 #
 user_name = input("Type your name: ")
 print(f"Hello {user_name}")
 #
-print(1+22)
-print(23-5)
-print(4*4)
-print(4**4)
-print(36/6)
-#
-print(36%6)
+# print(1+22)
+# print(23-5)
+# print(4*4)
+# print(4**4)
+# print(36/6)
+# #
+# print(36%6)
 #
 balance = 100
 amount_withdrawn = 30
@@ -36,5 +36,5 @@ print(balance)
 
 print("Type in two numbers to multiply them")
 num1 = int(input("Number 1: "))
-num2 = int(input("Number 2: "))
+num2 = int(input("Number 2: Na"))
 print(num1 * num2)
