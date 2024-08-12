51a1e9a9bdd4b9c7f2b4bd411b94f8b7658b45e0
MDN-DA <naveedm@programmer.net>
2024-08-12 10:52:02 +0100

    change notes

---
+++ Training/[w3]var.py
@@ -0,0 +1,40 @@
+print("All around the world"[7].upper())
+#
+my_name = "Gunners"
+print(my_name)
+print(my_name)
+print(my_name)
+print(my_name)
+#
+my_name = "Naveed"
+my_age = "36"
+is_student = True
+print("Naveed has completed the course")
+is_student = False
+print("My name is", my_name)
+#
+fav_hot_drink = "Espresso"
+fav_cold_drink = "Exotic Smoothie (Mango, Pineapple, Papapya)"
+print("My favorite drink is", fav_cold_drink)
+print(f"My name is {my_name}, my favorite drink is {fav_cold_drink}, my age is {my_age}")
+#
+user_name = input("Type your name: ")
+print(f"Hello {user_name}")
+#
+print(1+22)
+print(23-5)
+print(4*4)
+print(4**4)
+print(36/6)
+#
+print(36%6)
+#
+balance = 100
+amount_withdrawn = 30
+balance -= amount_withdrawn
+print(balance)
+#
+print("Type in two numbers to multiply them")
+num1 = int(input("1006"))
+num2 = int(input("225"))
+print(num1*num2)
