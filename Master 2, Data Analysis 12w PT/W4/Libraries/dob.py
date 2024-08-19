c7cf91a06b19796973a70741ffcf44d7af3d8a6b
MDN-DA <naveedm@programmer.net>
2024-08-19 17:24:27 +0100

    refn

--- Master 2, Data Analysis 12w PT/W4/Libraries/dob.py
+++ Master 2, Data Analysis 12w PT/W4/Libraries/dob.py
@@ -1,5 +1,5 @@
 import datetime
 
-dobt = datetime.datetime(1988, 10, 31)
-todayt = datetime.datetime.now()
-print(todayt-dobt)
+dob = datetime.datetime(1988, 10, 31)
+today = datetime.datetime.now()
+print(today-dob)
