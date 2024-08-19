ba800d8d4797b5644189663179aeece011362c5b
MDN-DA <naveedm@programmer.net>
2024-08-19 21:31:05 +0100

    refn.a2

--- Master 2, Data Analysis 12w PT/W4/Functions/bday.py
+++ Master 2, Data Analysis 12w PT/W4/Functions/bday.py
@@ -1,4 +1,7 @@
 def bday(username):
-    print(f"Happy birthday to you, Happy birthday to you, Happy birthday, dear {username}, Happy birthday to you!, Happy birthday to you, Happy birthday to you, Happy birthday, dear {username}, Happy birthday to you!")
+    print(f"Happy birthday to you, Happy birthday to you,"
+        "Happy birthday, dear {username}, Happy birthday to you!,"
+        "Happy birthday to you, Happy birthday to you,"
+        "Happy birthday, dear {username}, Happy birthday to you!")
 
 bday("Ayesha")
